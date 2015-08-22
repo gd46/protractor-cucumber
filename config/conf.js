6e4143927b14c5ca32a692fa383d58833822aa01
Giuseppe DiBella <gd46@njit.edu>
2015-08-21 20:38:59 -0400

    Updated protractor config

--- config/conf.js
+++ config/conf.js
@@ -1,15 +1,23 @@
 exports.config = {
   framework: 'cucumber',
+  cucumberOpts: {
+    format: 'pretty'
+  },
   seleniumAddress: 'http://127.0.0.1:4444/hub',
   seleniumPort: null,
   seleniumArgs: [],
   suites: {
-    homepage: 
+    homepage: './features/homepage.feature'
   },
   chromeOnly: true,
   chromeDriver: './node_modules/protractor/selenium/chromedriver',
   capabilities: {
     'browserName': 'chrome'
   },
-  baseUrl: ''
+  baseUrl: 'https://github.com/',
+  onPrepare: function() {
+    browser.driver.manage().window().maximize();
+    browser.ignoreSynchronization = true;
+  },
+  allScriptsTimeout: 30000
 }
