3c79b364e1b29d035916ed71be25545db1c96d02
Giuseppe DiBella <gd46@njit.edu>
2015-08-23 10:08:34 -0400

    Fixed error with protractor config

--- config/conf.js
+++ config/conf.js
@@ -3,11 +3,11 @@ exports.config = {
   cucumberOpts: {
     format: 'pretty'
   },
-  seleniumAddress: 'http://127.0.0.1:4444/hub',
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   seleniumPort: null,
   seleniumArgs: [],
   suites: {
-    homepage: './features/homepage.feature'
+    homepage: '../features/homepage.feature'
   },
   chromeOnly: true,
   chromeDriver: './node_modules/protractor/selenium/chromedriver',
